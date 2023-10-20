79a9c974e87ed0f80a90b6d6d538dd5ddcd968e7
Theresa Tran <theresa1194@gmail.com>
2023-10-20 11:37:56 +1100

    done

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+
+const [blackAndWhite, , black, pink, ] = farmAnimals.split(' ');
+
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [ r, o, y, g, b, , v] = colors;
+const [ , , , , , indg, ] = colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -24,6 +34,10 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+
+const {muppetName, color, song, job, partner} = muppet;
+const {album : {theMuppetMovie : {song2,song4}}, nestedJob, nestedPartner} = nestedMuppet;
 
 // Strings
 
